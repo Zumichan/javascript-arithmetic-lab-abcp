076d6bfdbc375d9e4c498e030b7f3ecf5cf4d2ad
Azumi Callanan <azucm@hotmail.com>
2018-01-13 18:21:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ function divide(a,b){
 }
 
 function inc(n){
-  return n++;
+  return n+ = 1;
 }
 
 function dec(n){
-  return n--;
+  return n- = 1;
 }
 
 function makeInt(n){
-  return parselnt(n,10);
+  return parseInt(n,10);
 }
 
 function preserveDecimal(n){
-  return parsefloat(n);
+  return parseFloat(n);
 }
